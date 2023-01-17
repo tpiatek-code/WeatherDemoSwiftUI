1e443c5ce6f7529145fc4223014dbe65743add94
Tomek Piątek <tpiatek.arch@icloud.com>
2023-01-17 08:43:37 +0100

    Initial Commit

---
+++ WeatherDemoSwiftUI/WeatherDemoSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherDemoSwiftUIApp.swift
+//  WeatherDemoSwiftUI
+//
+//  Created by Tomasz Piątek on 17/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct WeatherDemoSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
